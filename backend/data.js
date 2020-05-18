772efadbae9cef1d84c3360498d49996ac7ef833
Akash Shaw <aakashshaw88@gmail.com>
2020-05-18 10:14:29 +0530

    adding backend

---
+++ backend/data.js
@@ -0,0 +1,47 @@
+export default  {
+    products: [{
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 1299,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 180,
+        countInStock: 4
+    },
+    {   
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 980,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 110,
+        countInStock: 6
+    },
+    {   
+        _id: '3',
+        name: 'best pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 1320,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 40,
+        countInStock: 6
+    },
+    {   
+        _id: '4',
+        name: 'best pant',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 1220,
+        brand: 'Addidas',
+        rating: 4.0,
+        numReviews: 122,
+        countInStock: 6
+    }
+]
+}

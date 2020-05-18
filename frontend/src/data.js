0e473e2b706150ba5768d83957422fc9b5e383d9
Akash Shaw <aakashshaw88@gmail.com>
2020-05-18 10:12:28 +0530

    Adding Files

---
+++ frontend/src/data.js
@@ -0,0 +1,33 @@
+export default  {
+    products: [{
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 1299,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 180
+    },
+    {   
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 980,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 110
+    },
+    {   
+        _id: '3',
+        name: 'best pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 1320,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 40
+    }
+]
+}
